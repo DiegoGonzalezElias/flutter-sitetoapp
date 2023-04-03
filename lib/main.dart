ea6230386e84640f49edd263d1cabe6573d61f3e
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2023-04-03 12:50:05 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+//
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
