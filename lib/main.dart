755618005d9e89189381d92f257540abf853cd37
DiegoGonzalezElias <diegogonzalezelias@gmail.com>
2023-04-21 13:21:28 +0200

    onesignal commented

--- lib/main.dart
+++ lib/main.dart
@@ -51,14 +51,14 @@ main() async {
 
   //oneSignal notification
   //Remove this method to stop OneSignal Debugging
-  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+  /*  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
 
   OneSignal.shared.setAppId("YOUR_ONESIGNAL_ID");
 
 // The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
   OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
     print("Accepted permission: $accepted");
-  });
+  }); */
   //unil here: oneSignal notification
 }
 
